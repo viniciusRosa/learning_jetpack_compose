d6926abf4901d8f76e24758e68a52bbc543b17d7
viniciusrosa <vinicius.rosa@gmail.com>
2024-08-06 09:58:36 -0300

    first component

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -4,44 +4,106 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.testcompose.ui.theme.Pink40
+import com.example.testcompose.ui.theme.Purple80
+import com.example.testcompose.ui.theme.Teal200
 import com.example.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TestComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      TestComposeTheme {
+//        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//          Greeting(
+//            name = "Android",
+//            modifier = Modifier.padding(innerPadding)
+//          )
+//        }
+        Surface {
+          ProductItem()
         }
+      }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TestComposeTheme {
-        Greeting("Android")
+private fun ProductItem() {
+  Surface(shape = RoundedCornerShape(15.dp), shadowElevation =  4.dp) {
+    Column(
+      Modifier
+        .heightIn(250.dp, 280.dp)
+        .width(200.dp)
+    ) {
+      val imageSize = 100.dp
+      Box(
+        modifier = Modifier
+          .height(imageSize)
+          .fillMaxWidth()
+          .background(brush = Brush.horizontalGradient(colors = listOf(Pink40, Teal200)))
+      ) {
+        Image(
+          modifier = Modifier
+            .size(imageSize)
+            .offset(y = imageSize / 2)
+            .clip(shape = CircleShape)
+            .align(Alignment.BottomCenter),
+          painter = painterResource(id = R.drawable.ic_launcher_background),
+          contentDescription = "imagem"
+        )
+      }
+      Spacer(modifier = Modifier.height(imageSize / 2))
+      Column(Modifier.padding(16.dp),) {
+        Text(
+          text = LoremIpsum(50).values.first(),
+          modifier = Modifier.padding(bottom = 8.dp),
+          fontSize = 18.sp,
+          fontWeight = FontWeight(700),
+          maxLines = 2,
+          overflow = TextOverflow.Ellipsis
+        )
+        Text(
+          text = "R$14,99",
+          fontSize = 14.sp,
+          fontWeight = FontWeight(400)
+        )
+      }
     }
+
+  }
 }
