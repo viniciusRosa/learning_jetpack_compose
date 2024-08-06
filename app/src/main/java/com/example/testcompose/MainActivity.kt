f10cfc9b87c721b2d0940d0d88458f35251d1c8e
viniciusrosa <vinicius.rosa@gmail.com>
2024-08-06 14:15:32 -0300

    remove unused imports

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
