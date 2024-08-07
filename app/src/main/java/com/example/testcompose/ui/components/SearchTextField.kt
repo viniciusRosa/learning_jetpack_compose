a79cb72c90a760f6ba279d2efcc6ac85f265e864
viniciusrosa <vinicius.rosa@gmail.com>
2024-08-07 14:35:23 -0300

    remove unused imports

--- app/src/main/java/com/example/testcompose/ui/components/SearchTextField.kt
+++ app/src/main/java/com/example/testcompose/ui/components/SearchTextField.kt
@@ -11,17 +11,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
-import com.example.testcompose.model.Product
 import com.example.testcompose.ui.theme.TestComposeTheme
-import java.math.BigDecimal
 
 @Composable
 fun SearchTextField(
